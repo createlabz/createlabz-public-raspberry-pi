f64660d9628f55b30d7f002af98fac2a1f9f8a6b
cjeminico <73282640+cjeminico@users.noreply.github.com>
2021-03-04 15:54:03 +0800

    Modify receive-sms.sh

--- usb_modem_dongle_huwaei_e173/gammu/receive/receive-sms.sh
+++ usb_modem_dongle_huwaei_e173/gammu/receive/receive-sms.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# Send texted message
+# Reply texted message
 from=$SMS_1_NUMBER
 message=$SMS_1_TEXT
 reply="Message Send $message"
-
 echo "$reply" | gammu-smsd-inject TEXT "$from"
 
 # Store to logs
-log=/home/pi/bin/sms/log_sms.txt
+log=/home/pi/createlabz-public-raspberry-pi/usb_modem_dongle_huwaei_e173/gammu/receive/
 date >> $log
 echo $from >> $log
 echo $message >> $log
